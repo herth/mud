f82419f36a04db0e241fa98e8c726787b457f3b7
Peter Herth <herth@peter-herth.de>
2016-02-21 21:29:02 +0100

    Input parser

--- mud.go
+++ mud.go
@@ -461,20 +461,104 @@ func loadAreas() {
 	//fmt.Println(areas)
 }
 
+func readQuoted(reader *bufio.Reader, delim rune) (result string, eof bool) {
+	var b bytes.Buffer
+	esc := false
+
+	for {
+		r, s, _ := reader.ReadRune()
+		if s == 0 {
+			eof = true
+			break
+		}
+		if esc {
+			switch r {
+			case 'n':
+				b.WriteRune(10)
+			case 't':
+				b.WriteRune(8)
+			default:
+				b.WriteRune(r)
+			}
+			esc = false
+		} else {
+			switch r {
+			case delim:
+				return b.String(), eof
+			case '\\':
+				esc = true
+			default:
+				b.WriteRune(r)
+			}
+		}
+
+	}
+	return b.String(), eof
+}
+
+func readExpr(reader *bufio.Reader) (tokens []string, eof bool) {
+	var b bytes.Buffer
+	//eof = skipWS(reader)
+loop:
+	for {
+		r, s, _ := reader.ReadRune()
+		if s == 0 {
+			eof = true
+			break
+		}
+		switch r {
+		case 10:
+			break loop
+		case 13:
+			continue
+		case 32, 9:
+			if b.Len() > 0 {
+				tokens = append(tokens, b.String())
+				b.Reset()
+			}
+			skipWS(reader)
+			continue
+		case '"', 39:
+			res, _ := readQuoted(reader, r)
+			tokens = append(tokens, res)
+			b.Reset()
+			continue
+		case '{':
+			res, _ := readQuoted(reader, '}')
+			tokens = append(tokens, res)
+			b.Reset()
+			continue
+		}
+		b.WriteRune(r)
+	}
+	if b.Len() > 0 {
+		tokens = append(tokens, b.String())
+	}
+	return
+}
+
 func handle(c net.Conn) {
 	//buffer := make([]byte, 1024)
 	player := Player{Name: "foo", Connection: c}
 	world.Players = append(world.Players, player)
-	scanner := bufio.NewScanner(c)
-	log.Printf("New connection from %v\n", c.RemoteAddr())
-	for scanner.Scan() {
-		line := scanner.Text()
-		log.Println("read:", line)
+
+	r := bufio.NewReader(c)
+
+	for {
+		tokens, eof := readExpr(r)
+		fmt.Println("read")
+		for i, tok := range tokens {
+			fmt.Println(i, tok, len(tok))
+		}
+		if len(tokens) == 1 && tokens[0] == "quit" {
+			c.Close()
+			break
+		}
+		if eof {
+			break
+		}
 	}
-	err := scanner.Err()
-	if err != nil {
-		log.Println(err)
-	}
+
 	log.Printf("socket closed %v\n", c.RemoteAddr())
 }
 
